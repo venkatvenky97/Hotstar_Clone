8678dae4cc4ab98127b5470af80a10718a327716
venkatvenky97 <virathvenkatesh@gmail.com>
2021-05-08 19:26:39 +0530

    Add file

---
+++ src/Components/Links.js
@@ -0,0 +1,6 @@
+export const mediaa = [
+  {
+    image:
+      "https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_1x/sources/r1/cms/prod/old_images/MOVIE/1352/1000081352/1000081352-h",
+  },
+];
