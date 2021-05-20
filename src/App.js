33c9e4392036764128885b2df1749b8efcbcadc7
venkatesh <79562400+venkatvenky97@users.noreply.github.com>
2021-05-20 15:47:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./Components/Header/Header";
 import MediaAd from "./Components/MediaAd/MediaAd";
 import Movies from "./Components/Moviess/Movies";
 import Media from "./Components/Media/Media";
-import DataLayer from "./DataLayer";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
@@ -13,9 +12,7 @@ function App() {
       <Header />
       <MediaAd />
       <Movies />
-      <DataLayer />
       <Media />
-      <DataLayer />
       <Footer />
     </div>
   );
